439510d9023611f695b2368b17941aca86114458
Voltar <walter.coggeshall@gmail.com>
2014-10-29 18:39:54 -0700

    Added assets folder including bar with simple texture.

---
+++ Assets/UIBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Scaling will only scale 1-1, if you increase both dimensions. If you want it longer, you have to edit the texture.
+public class UIBar : MonoBehaviour {
+	public int sizex;
+	public int sizey;
+	public int barToDisplayPercent;
+	public Texture2D progbarempty;
+	public Texture2D progbarfull;
+	void OnGUI(){
+		//RGB+Alpha. This affects ALL UI elements. Workaround until UISkin materializes in the assets directory.
+		GUI.backgroundColor = new Color (1.0f, 1.0f,1.0f, 0.0f);
+		//draw the background
+		GUI.BeginGroup (new Rect (10, 10, sizex, sizey));
+			GUI.Box (new Rect (0, 0, sizex, sizey), progbarempty);
+			//draw the filling
+			GUI.BeginGroup (new Rect (0, 0, sizex * barToDisplayPercent/100, sizey));
+				GUI.Box (new Rect (0, 0, sizex - 2, sizey - 2), progbarfull);
+			GUI.EndGroup ();
+		GUI.EndGroup ();
+
+
+	}
+}
