e231e0f7b4130dc7d481249702d4d7e349742772
Cheddar2012 <cheddar1@outlook.com>
2014-11-03 13:12:18 -0800

    Super basic player movement with placeholder sprite

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed, 0));
+	}
+}
