5e428fe94e75dfc5a2304bc303f9009db2d19b98
Cheddar2012 <cheddar1@outlook.com>
2014-11-05 14:41:42 -0800

    Added minimap and camera tracking

---
+++ Assets/ActivateMap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateMap : MonoBehaviour {
+
+	public Camera fullMapCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI () {
+		if (Input.GetKey(KeyCode.M)) {
+			fullMapCamera.gameObject.SetActive(true);
+		} else {
+			fullMapCamera.gameObject.SetActive(false);
+		}
+	}
+}
