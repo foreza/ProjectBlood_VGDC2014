2433f2493f9375a5f45634e13bfaf2ca09db265b
Cheddar2012 <cheddar1@outlook.com>
2014-11-07 18:58:01 -0800

    Doors and switches

---
+++ Assets/Scripts/DoorSwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSwitch : MonoBehaviour {
+
+	public GameObject door; // The gate this switch affects
+	
+	public bool canOpen = true; // Can this switch open the gate?
+	public bool canClose = false; // Can this switch close the gate?
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+				door.GetComponent<DoorScript>().ToggleDoor();
+		}
+	}
+}
