16a15c4f98c836b7e43d4827d3456e6bd2646f44
djgonza1 <djgonza1@uci.edu>
2014-11-14 17:58:25 -0800

    Added patrolling and line of sight events for AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,25 +1,87 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum EnemyState
+{
+	PATROL,
+	CHASING
+}
+
 public class Enemy : Character 
 {
 	public Player player;
-
+	public GameObject[] patrolPath;
+	public EnemyState state;
+	
+	void Awake()
+	{
+		this.health = 100;
+		this.speed = 5;
+	}
 	void Start () 
 	{
-	
+		StartCoroutine("Patrol");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		followPlayer();
+		//followPlayer();
+		
 	}
-
-	void followPlayer()
+	
+	IEnumerator FollowPlayer()
 	{
-		Vector3 direction = player.gameObject.transform.position - this.gameObject.transform.position;
-		Debug.Log("x: " + direction.x +  "  y: " + direction.y);
-		this.gameObject.transform.Translate(Vector3.ClampMagnitude(direction, speed*Time.deltaTime));
+		this.state = EnemyState.CHASING;
+		while(this.state == EnemyState.CHASING)
+		{
+			Vector3 direction = player.transform.position - this.transform.position;
+			this.transform.Translate(Vector3.ClampMagnitude(direction, speed*Time.deltaTime), Space.World);
+			this.transform.right = player.transform.position - this.transform.position;
+			yield return null;
+		}
+	}
+	
+	IEnumerator Patrol()
+	{
+		state = EnemyState.PATROL;
+		
+		int i = 0;
+		while(state == EnemyState.PATROL)
+		{
+			
+			Vector2 to = patrolPath[i].transform.position;
+			
+			while( (Vector2)this.transform.position != to && state == EnemyState.PATROL )
+			{
+				Vector2 from = this.transform.position;
+				float fracJourney = speed * Time.deltaTime / (to - from).magnitude;
+				//distTravel = distTravel + fracJourney;
+				this.transform.position = Vector2.Lerp(from,to,fracJourney);
+				this.transform.right = to - from;
+				yield return null;
+			}
+			
+			
+			i = (i >= patrolPath.Length - 1)? 0 : ++i;
+		}
+	}
+	
+	public void OnPlayerSighted()
+	{
+		if(state != EnemyState.CHASING)
+		{
+			StartCoroutine("FollowPlayer");
+		}
+		
+	}
+	
+	public void OnPlayerLost()
+	{
+		if(state == EnemyState.CHASING)
+		{
+			StartCoroutine("Patrol");
+		}
 	}
 }
+
---
+++ Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineOfSight : MonoBehaviour
+{
+	private Enemy enemy;
+	
+	void Start()
+	{
+		enemy = this.transform.parent.gameObject.GetComponent<Enemy>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			enemy.OnPlayerSighted();
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			enemy.OnPlayerLost();
+		}
+	}
+}
