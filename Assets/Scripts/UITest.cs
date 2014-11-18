f7115414d8eda0ee2942b207fd8926f486639660
Voltar <walter.coggeshall@gmail.com>
2014-11-17 20:47:39 -0800

    Got basic, buggy procedural UI working.

--- Assets/Scripts/UITest.cs
+++ Assets/Scripts/UITest.cs
@@ -5,16 +5,23 @@ using UnityEngine.UI;
 public class UITest : MonoBehaviour {
 	public Slider healthbarslider;
 	public Slider energybarslider;
+	public GameObject objectivesPanel;
+
 	void setBar(Slider bar,float amount){
 		bar.value = amount;
 		}
-	// Use this for initialization
-	void Start () {
-	
+	void OnGUI(){
+		if(Input.GetKeyDown("tab")){
+			objectives ();
+		}
+	}
+	public Text obj1;
+	public Font font1;
+	void objectives(){
+		obj1 = gameObject.AddComponent<Text> ();
+		obj1.text = "ASDFASDF";
+		obj1.font = font1;
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
