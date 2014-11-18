5456bdf371e6be172269a52800be16f333aa3598
Voltar <walter.coggeshall@gmail.com>
2014-11-17 22:32:35 -0800

    Got UI really, really working. Still only displays one thing.

--- Assets/Scripts/UITest.cs
+++ Assets/Scripts/UITest.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
@@ -6,6 +6,7 @@ public class UITest : MonoBehaviour {
 	public Slider healthbarslider;
 	public Slider energybarslider;
 	public GameObject objectivesPanel;
+	public GameObject oneObjective;
 
 	void setBar(Slider bar,float amount){
 		bar.value = amount;
@@ -18,9 +19,27 @@ public class UITest : MonoBehaviour {
 	public Text obj1;
 	public Font font1;
 	void objectives(){
+
+		
+		RectTransform containerRectTransform = gameObject.GetComponent<RectTransform>();
+		float width = containerRectTransform.rect.width;
+		float height = containerRectTransform.rect.height;
+		GameObject theobj = Instantiate (oneObjective) as GameObject;
+		theobj.transform.parent = gameObject.transform;
+		RectTransform rectTransform = theobj.GetComponent<RectTransform>();
+	
+		float x = 350;
+		float y = -100;
+		rectTransform.offsetMin = new Vector2(x, y);
+		
+		x = rectTransform.offsetMin.x + width;
+		y = rectTransform.offsetMin.y + height;
+		rectTransform.offsetMax = new Vector2(x, y);
+
 		obj1 = gameObject.AddComponent<Text> ();
 		obj1.text = "ASDFASDF";
 		obj1.font = font1;
+
 
 	}
 	
