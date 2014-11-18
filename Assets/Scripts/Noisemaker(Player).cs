38f0c3ecbe7c13257fe13e66e294c98a2e5ba46e
Reddeyfish <edrichd@uci.edu>
2014-11-17 21:02:10 -0800

    Revert "Revert "Fixed Noisemaker""
    This reverts commit dbf82c17a8b5129c45d64b2100e91e8899654320.

--- Assets/Scripts/Noisemaker(Player).cs
+++ Assets/Scripts/Noisemaker(Player).cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using System.Collections;
 
-public class Noisemaker(Player) : MonoBehaviour {
 
+
+public class NoisemakerPlayer : MonoBehaviour {
+	private string[] layers = {"Mobs"};
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +13,17 @@ public class Noisemaker(Player) : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void Noise(float radius)
+	{
+		RaycastHit2D[] hits  = Physics2D.CircleCastAll(transform.position, radius, new Vector2(0,0), 0, LayerMask.GetMask(layers));
+		foreach (RaycastHit2D hit in hits)
+		{
+			if (hit.collider.tag == "Enemy") {
+				Debug.Log ("Did you hear that?", hit.collider);
+			}
+		}
 	}
 }
 
