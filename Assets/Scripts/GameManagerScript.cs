0fd2cd8a165706764b48884907a5725f0b9df55b
MrBlock <il0ver0se@yahoo.com>
2014-11-17 21:32:09 -0800

    added a timer and pause function

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManagerScript : MonoBehaviour {
 
+	public Text timerText;
+	public float timer = 60;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,8 +13,18 @@ public class GameManagerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		timer -= Time.deltaTime;
+		timer = Mathf.Max (0, timer);
+		timerText.text = (timer).ToString();
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			Application.LoadLevel("MainMenu");
 				}
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (Time.timeScale == 0)
+				Time.timeScale = 1.0f;
+			else
+				Time.timeScale = 0;
+			
+		}
 	}
 }
