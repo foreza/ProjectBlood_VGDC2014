736c1f5e58678a8b27960c2979f21b1b89b6bbf8
Cheddar2012 <cheddar1@outlook.com>
2014-11-17 22:47:59 -0800

    Restored GameManagerScript

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManagerScript : MonoBehaviour {
-
+	
+	public Text timerText;
+	public float timer = 60;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		timer -= Time.deltaTime;
+		timer = Mathf.Max (0, timer);
+		timerText.text = (timer).ToString();
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			Application.LoadLevel("MainMenu");
-				}
+		}
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (Time.timeScale == 0)
+				Time.timeScale = 1.0f;
+			else
+				Time.timeScale = 0;
+		}
 	}
 }
