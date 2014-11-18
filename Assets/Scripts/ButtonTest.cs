b53a40eb27e25e74104395f16196c448955b1848
MrBlock <il0ver0se@yahoo.com>
2014-11-17 19:50:29 -0800

    Added Main Menu Scene and Script

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTest : MonoBehaviour {
+	
+	public void buttonClick(){
+		Debug.Log ("Button Clicked");
+		Application.LoadLevel(1);
+	}
+	public void startLevel(){
+		Application.LoadLevel(1);
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
