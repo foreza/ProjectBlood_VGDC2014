341a972ba82e7cfbe82181af0dc1078b456491f9
Reddeyfish <edrichd@uci.edu>
2014-11-22 14:55:46 -0800

    Added LoS checking function to the sound system
    (you might want this functionality for some of the other systems, such
    as the enemy detection system. What's the best way of doing that?)

--- Assets/Scripts/NoisemakerPlayer.cs
+++ Assets/Scripts/NoisemakerPlayer.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 
 public class NoisemakerPlayer : MonoBehaviour {
-	private string[] layers = {"Mobs"};
+	private string[] soundLayers = {"Enemy"};
+	private string[] lightLayers = {"Enemy","LightWalls"};
 	// Use this for initialization
 	void Start () {
 	
@@ -12,18 +13,55 @@ public class NoisemakerPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Noise (99999);
 	}
 
 	public void Noise(float radius)
 	{
-		RaycastHit2D[] hits  = Physics2D.CircleCastAll(transform.position, radius, new Vector2(0,0), 0, LayerMask.GetMask(layers));
+		RaycastHit2D[] hits  = Physics2D.CircleCastAll(transform.position, radius, new Vector2(0,0), 0, LayerMask.GetMask(soundLayers));
 		foreach (RaycastHit2D hit in hits)
 		{
 			if (hit.collider.tag == "Enemy") {
 				Debug.Log ("Did you hear that?", hit.collider);
+				if(hasLoS (transform, hit.transform, (int)radius, LayerMask.GetMask(lightLayers)))
+				{
+					Debug.Log ("Did you see that?", hit.collider);
+				}
 			}
 		}
+	}
+	public bool hasLoS(Transform one, Transform two, int distance, LayerMask mask)
+	{
+		Vector3 rayDirection = two.position - one.position;
+		RaycastHit2D hit = Physics2D.Raycast (one.position, rayDirection, distance, mask);
+		
+		if(hit.transform.Equals (two)) //there is nothing else that it hit between them
+		{
+			return true;
+		}
+		return false;
+	}
+	public bool hasLoS(Transform one, Transform two, LayerMask mask)
+	{
+		Vector3 rayDirection = two.position - one.position;
+		RaycastHit2D hit = Physics2D.Raycast (one.position, rayDirection, int.MaxValue, mask);
+
+		if(hit.transform.Equals (two)) //there is nothing else that it hit between them
+		{
+				return true;
+		}
+		return false;
+	}
+	public bool hasLoS(Transform one, Transform two) //don't use this if Transform one has it's own collider; it'll collide with itself
+	{
+		Vector3 rayDirection = two.position - one.position;
+		RaycastHit2D hit = Physics2D.Raycast (one.position, rayDirection);
+
+		if(hit.transform.Equals (two)) //there is nothing else that it hit between them
+		{
+			return true;
+		}
+		return false;
 	}
 }
 
