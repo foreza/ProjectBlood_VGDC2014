5fc12b0ef194bf4a8149ee091289fba5caef6bc0
Lawrence Chen <lawrenceochen@gmail.com>
2015-01-19 15:38:50 -0800

    Changed Sword.cs to now use OnTriggerEnter2D

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public enum SwordState
 {
-	STANDBY,
-	SWINGING
+    STANDBY,
+    SWINGING
 }
 
 public class Sword : Weapon
 {
-	public float swingTime = 5.0f;
-	public SwordState state = SwordState.STANDBY;
+    public float swingTime = 5.0f;
+    public SwordState state = SwordState.STANDBY;
     public float damage = 50.0f;
     private NoisemakerPlayer noisemaker;
 
@@ -22,60 +22,60 @@ public class Sword : Weapon
 
     }
 
-	public override void Attack()
-	{
-		Swing ();
-	}
+    public override void Attack()
+    {
+        Swing();
+    }
 
-	public void Swing()
-	{
-		if(this.renderer.enabled == false)
-		{
-			this.audio.Play();
-			StartCoroutine("SwingMotion");
+    public void Swing()
+    {
+        if (this.renderer.enabled == false)
+        {
+            this.audio.Play();
+            StartCoroutine("SwingMotion");
             noisemaker.Noise(9999999);
-		}
-	}
+        }
+    }
 
-	public override void Unsheathe(bool swordOut)
-	{
-		this.renderer.enabled = swordOut;
-		this.collider2D.enabled = swordOut;
-	}
+    public override void Unsheathe(bool swordOut)
+    {
+        this.renderer.enabled = swordOut;
+        this.collider2D.enabled = swordOut;
+    }
 
-	IEnumerator SwingMotion()
-	{
-		state = SwordState.SWINGING;
+    IEnumerator SwingMotion()
+    {
+        state = SwordState.SWINGING;
 
-		this.renderer.enabled = true;
-		this.collider2D.enabled = true;
+        this.renderer.enabled = true;
+        this.collider2D.enabled = true;
 
-		float currentTime = 0.0f;
-		while(state == SwordState.SWINGING)
-		{
-			currentTime = currentTime + Time.deltaTime;
-			if(currentTime >= this.swingTime)
-			{
-				this.renderer.enabled = false;
-				this.collider2D.enabled = false;
-				state = SwordState.STANDBY;
-			}
-			yield return null;
-		}
+        float currentTime = 0.0f;
+        while (state == SwordState.SWINGING)
+        {
+            currentTime = currentTime + Time.deltaTime;
+            if (currentTime >= this.swingTime)
+            {
+                this.renderer.enabled = false;
+                this.collider2D.enabled = false;
+                state = SwordState.STANDBY;
+            }
+            yield return null;
+        }
 
-	}
+    }
 
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		if(other.gameObject.tag == "Enemy")
-		{
-			other.gameObject.GetComponent<Enemy>().GetHit(damage);
-		}
-		if(other.gameObject.tag == "EnemyBoss")
-		{
-			other.gameObject.SetActive(false); //replace with actual damage system
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<Enemy>().GetHit(damage);
+        }
+        if (other.gameObject.tag == "EnemyBoss")
+        {
+            other.gameObject.SetActive(false); //replace with actual damage system
             Application.LoadLevel(4);
-		}
+        }
 
-	}
+    }
 }
