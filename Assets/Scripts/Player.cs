b8b2c123619bffbab5178f48fcce494cefaec65d
Lawrence Chen <lawrenceochen@gmail.com>
2015-01-20 10:45:19 -0800

    Sword changes
    - Sword is now its own prefab.
    - Sword now has a (crude) animation when active.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,141 +3,141 @@ using System.Collections;
 
 public enum PlayerState
 {
-	NORMAL,
-	STEALTH,
+    NORMAL,
+    STEALTH,
 }
 
 
-public class Player : Character 
+public class Player : Character
 {
-	public float energy = 50;
-	public float energyMax = 50;
-	public float energyRegenRate = 1.0f;
-	public bool energyRegen = true;
-	public PlayerState state;
-	public SpriteRenderer sprite;
-	public Weapon weapon;
-	public AudioClip dmgClip;
-	public AudioClip stealthClip;
+    public float energy = 50;
+    public float energyMax = 50;
+    public float energyRegenRate = 1.0f;
+    public bool energyRegen = true;
+    public PlayerState state;
+    public SpriteRenderer sprite;
+    public Weapon weapon;
+    public AudioClip dmgClip;
+    public AudioClip stealthClip;
 
-	private float stealthDegenRate = 10.0f;
+    private float stealthDegenRate = 10.0f;
 
-	void Start () 
-	{
-		sprite = transform.FindChild ("PlayerPlaceholder").GetComponent<SpriteRenderer>();
-		weapon = GameObject.Find ("Sword").GetComponent<Sword> ();
-		state = PlayerState.NORMAL;
-	}
+    void Start()
+    {
+        sprite = transform.FindChild("PlayerPlaceholder").GetComponent<SpriteRenderer>();
+        weapon = this.transform.FindChild("Sword").GetComponent<Sword>();
+        state = PlayerState.NORMAL;
+    }
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		if(this.health <= 0)
-		{
-			killPlayer();
-		}
-		if (energyRegen)
-		{
-			if (energy <= energyMax)
-				energy += (energyRegenRate * Time.deltaTime);
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.health <= 0)
+        {
+            killPlayer();
+        }
+        if (energyRegen)
+        {
+            if (energy <= energyMax)
+                energy += (energyRegenRate * Time.deltaTime);
+        }
+    }
 
-	void killPlayer()
-	{
-		this.gameObject.SetActive(false);
-		Application.LoadLevel(3);
-		// StartCoroutine("LoadStartScreen"); 
+    void killPlayer()
+    {
+        this.gameObject.SetActive(false);
+        Application.LoadLevel(3);
+        // StartCoroutine("LoadStartScreen"); 
 
-	}
+    }
 
-	void takeHit(float dmg)
-	{
-		this.health = this.health - dmg;
-		if(this.health < 0)
-		{
-			this.health = 0;
-		}
-	}
+    public void takeHit(float dmg)
+    {
+        this.health = this.health - dmg;
+        if (this.health < 0)
+        {
+            this.health = 0;
+        }
+    }
 
-	public void Demacia()
-	{
-		StartCoroutine("DemaciaRoutine");
-	}
+    public void Demacia()
+    {
+        StartCoroutine("DemaciaRoutine");
+    }
 
-	IEnumerator DemaciaRoutine()
-	{
-		while(Input.GetButton("Demacia"))
-		{
-			this.weapon.Unsheathe(true);
-			this.gameObject.transform.Rotate(Vector3.forward, 30.0f, Space.Self);
-			yield return null;
-		}
+    IEnumerator DemaciaRoutine()
+    {
+        while (Input.GetButton("Demacia"))
+        {
+            this.weapon.Unsheathe(true);
+            this.gameObject.transform.Rotate(Vector3.forward, 30.0f, Space.Self);
+            yield return null;
+        }
 			
-		this.weapon.Unsheathe(false);
-	}
+        this.weapon.Unsheathe(false);
+    }
 
-	public void Stealth()
-	{
-		if (this.state == PlayerState.NORMAL) 
-		{
-			StartCoroutine("StealthRoutine");
-		}
-		else if (this.state == PlayerState.STEALTH)
-		{
-			//make a function for this later
-			this.energyRegen = true;
-			this.sprite.enabled = true;
-			this.state = PlayerState.NORMAL;
-			this.gameObject.audio.Play ();
-		}
-	}
+    public void Stealth()
+    {
+        if (this.state == PlayerState.NORMAL)
+        {
+            StartCoroutine("StealthRoutine");
+        } else if (this.state == PlayerState.STEALTH)
+        {
+            //make a function for this later
+            this.energyRegen = true;
+            this.sprite.enabled = true;
+            this.state = PlayerState.NORMAL;
+            this.gameObject.audio.Play();
+        }
+    }
 	
-	IEnumerator StealthRoutine()
-	{
+    IEnumerator StealthRoutine()
+    {
 		
-		state = PlayerState.STEALTH;
-		//		float stealthTime = player.meldTime;
-		sprite.enabled = false;
-		energyRegen = false;
-		audio.clip = this.stealthClip;
-		this.audio.Play ();
+        state = PlayerState.STEALTH;
+        //		float stealthTime = player.meldTime;
+        sprite.enabled = false;
+        energyRegen = false;
+        audio.clip = this.stealthClip;
+        this.audio.Play();
 		
-		//		float currentTime = 0.0f;
+        //		float currentTime = 0.0f;
 		
-		while(this.state == PlayerState.STEALTH)
-		{
-			this.energy -= stealthDegenRate*Time.deltaTime;
-			if (this.energy <= 0)
-			{
-				this.energy = 0;
-				this.energyRegen = true;
-				this.sprite.enabled = true;
-				this.state = PlayerState.NORMAL;
-				this.audio.Play ();
-			}
-			yield return null;
-		}
-	}
+        while (this.state == PlayerState.STEALTH)
+        {
+            this.energy -= stealthDegenRate * Time.deltaTime;
+            if (this.energy <= 0)
+            {
+                this.energy = 0;
+                this.energyRegen = true;
+                this.sprite.enabled = true;
+                this.state = PlayerState.NORMAL;
+                this.audio.Play();
+            }
+            yield return null;
+        }
+    }
 
-	void OnCollisionEnter2D(Collision2D coll)
-	{
-		if(coll.gameObject.tag == "Enemy")
-		{
-			takeHit(10);
-			this.audio.clip = this.dmgClip;
-			//this.audio.Play();
-			Debug.Log("Health: " + this.health);
-		}
-	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            takeHit(10);
+            this.audio.clip = this.dmgClip;
+            //this.audio.Play();
+            Debug.Log("Health: " + this.health);
+        }
+    }
 
-	IEnumerator LoadStartScreen() { // not working as intended oh well.
+    IEnumerator LoadStartScreen()
+    { // not working as intended oh well.
 	
-		yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(1);
 
 
-		Application.LoadLevel(0);
-	}
+        Application.LoadLevel(0);
+    }
 
 
 }
--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -12,14 +12,19 @@ public class Sword : Weapon
     public float swingTime = 5.0f;
     public SwordState state = SwordState.STANDBY;
     public float damage = 50.0f;
+    public Transform handlePosition;
     private NoisemakerPlayer noisemaker;
+    private Character swordWielder;
+    private float swingSpeed = 1000;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
 
     // Use this for initialization
     void Start()
     {
         noisemaker = transform.Find("ParticleEffects").GetComponent<NoisemakerPlayer>();
-
-
+        swordWielder = transform.parent.GetComponent<Character>(); 
+        handlePosition = this.transform.parent.FindChild("HandlePosition");
     }
 
     public override void Attack()
@@ -32,6 +37,8 @@ public class Sword : Weapon
         if (this.renderer.enabled == false)
         {
             this.audio.Play();
+            originalPosition = this.transform.localPosition;
+            originalRotation = this.transform.localRotation;
             StartCoroutine("SwingMotion");
             noisemaker.Noise(9999999);
         }
@@ -51,14 +58,22 @@ public class Sword : Weapon
         this.collider2D.enabled = true;
 
         float currentTime = 0.0f;
+ 
         while (state == SwordState.SWINGING)
         {
-            currentTime = currentTime + Time.deltaTime;
+            currentTime += Time.deltaTime;
+           
             if (currentTime >= this.swingTime)
             {
                 this.renderer.enabled = false;
                 this.collider2D.enabled = false;
                 state = SwordState.STANDBY;
+                //Get the sword back to its original position and rotation once the sword swing is done
+                this.transform.localPosition = originalPosition;
+                this.transform.localRotation = originalRotation;
+            } else {
+                //simple sword swing until animations are working
+                transform.RotateAround(handlePosition.position, this.transform.forward, Time.deltaTime * swingSpeed);
             }
             yield return null;
         }
@@ -67,14 +82,22 @@ public class Sword : Weapon
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Enemy")
+
+        if (swordWielder.tag == "Player")
         {
-            other.gameObject.GetComponent<Enemy>().GetHit(damage);
-        }
-        if (other.gameObject.tag == "EnemyBoss")
+            if (other.gameObject.tag == "Enemy")
+            {
+                other.gameObject.GetComponent<Enemy>().GetHit(damage);
+            } 
+            else if (other.gameObject.tag == "EnemyBoss")
+            {
+                other.gameObject.SetActive(false); //replace with actual damage system
+                Application.LoadLevel(4);
+            }
+        } 
+        else if ((swordWielder.tag == "Enemy" || swordWielder.tag == "EnemyBoss") && other.gameObject.tag == "Player")
         {
-            other.gameObject.SetActive(false); //replace with actual damage system
-            Application.LoadLevel(4);
+            other.gameObject.GetComponent<Player>().takeHit(damage);
         }
 
     }
