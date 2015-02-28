c886a6decfdf8f1c69713160d2df54eb61f51da2
Reddeyfish <edrichd@uci.edu>
2015-02-27 17:00:52 -0800

    added teleport skill

--- Assets/Scripts/AbilityScripts/RandomTeleport.cs
+++ Assets/Scripts/AbilityScripts/RandomTeleport.cs
@@ -4,18 +4,37 @@ using System.Collections;
 public class RandomTeleport : AbstractSkill {
     public Transform teleportOrigin;
     public float maxRange;
+    private float timer = 0;
+    public float cooldown = 10;
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+	    if (timer > 0) {
+			timer += Time.deltaTime;
+			if (timer >= cooldown) {
+				timer = 0;
+			}
+		}
 	}
 
     public override void Use()
     {
-
+        if (timer > 0) //the ability is still on cooldown
+        {
+            return;
+        }
+        Vector2 teleportDirection = Random.insideUnitCircle * maxRange;
+        Vector2 newTestPosition = (Vector2)teleportOrigin.position  + teleportDirection;
+        while (Physics2D.OverlapCircle(newTestPosition, (GetComponent<CircleCollider2D>().radius))) //while position is invalid, make a new position
+        {
+            teleportDirection = Random.insideUnitCircle * maxRange;
+            newTestPosition = (Vector2)(transform.position) + teleportDirection;
+        }
+        transform.position = newTestPosition;
+        Debug.Log("Teleport!");
+        timer += Time.deltaTime;
     }
 }
--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public enum EnemyState
 {
@@ -39,7 +40,7 @@ public class Enemy : Character
 	private float attackTimer = 0.0f;
 	private float ATTACK_DAMAGE = 10.0f;
 	public float DISTANCE_TO_ATTACK = 50.0f;
-
+    private List<AbstractSkill> abilities;
 	private Weapon weapon;
 
 	// INITIALIZE
@@ -58,6 +59,13 @@ public class Enemy : Character
 		deathParticleEffect = transform.FindChild ("EnemyPlaceholder").GetComponent<ParticleSystem>();
 
 		weapon = transform.FindChild("Sword").GetComponent<Sword>();
+        
+        abilities = new List<AbstractSkill>();
+        AbstractSkill probAbility = transform.GetComponent<RandomTeleport>();
+        if (probAbility != null)
+        {
+            abilities.Add(probAbility);
+        }
 	}
 
 	// FIXED UPDATE
@@ -94,6 +102,10 @@ public class Enemy : Character
 			if (distanceToPlayer <= DISTANCE_TO_ATTACK) {
 				state = EnemyState.ATTACK;
 			}
+            foreach (AbstractSkill ability in abilities)
+            {
+                ability.Use();
+            }
 		}
 		else 														// ... Otherwise, try to find the breadcrumbs.
 		{
@@ -135,9 +147,10 @@ public class Enemy : Character
 		}
 		else if (attackTimer == 0) {
 			// player.GetComponent<Character>().health -= ATTACK_DAMAGE; // deals damage to player
+            
 			weapon.Attack();
 			attackTimer += Time.deltaTime;
-            LookTowards(player.transform.position); //face the player
+            //LookTowards(player.transform.position); //face the player
 
 		} else if (attackTimer > 0) {
 			attackTimer += Time.deltaTime;
