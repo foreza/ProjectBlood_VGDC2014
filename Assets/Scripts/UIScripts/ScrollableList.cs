d4e5cea7aa378491c7018ab8761ded3c4ad4da62
Walter Coggeshall <walter.coggeshall@gmail.com>
2015-02-27 16:52:29 -0800

    Frontend works in FinalBloodBuild scene

---
+++ Assets/Scripts/UIScripts/ScrollableList.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ScrollableList : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public int columnCount = 1;
+	List<Objective> descs;
+	public int itemCount;
+    public void doTheGUI()
+    {
+		descs = this.GetComponent<UIMaster> ().descriptions;
+		itemCount = descs.Count;
+        RectTransform rowRectTransform = itemPrefab.GetComponent<RectTransform>();
+        RectTransform containerRectTransform = gameObject.GetComponent<RectTransform>();
+
+        //calculate the width and height of each child item.
+        float width = containerRectTransform.rect.width / columnCount; //half screen width
+        float ratio = width / rowRectTransform.rect.width;
+        float height = rowRectTransform.rect.height * ratio; //distance between two rows
+        int rowCount = itemCount / columnCount;
+        //if (itemCount % rowCount > 0)
+          //  rowCount++;
+
+        //adjust the height of the container so that it will just barely fit all its children
+        float scrollHeight = height * rowCount;
+        containerRectTransform.offsetMin = new Vector2(containerRectTransform.offsetMin.x, -scrollHeight / 2);
+        containerRectTransform.offsetMax = new Vector2(containerRectTransform.offsetMax.x, scrollHeight / 2);
+
+        int j = 0;
+		Debug.Log ("objectives has length "+descs.Count);
+        for (int i = 0; i < itemCount; i++)
+        {
+            //this is used instead of a double for loop because itemCount may not fit perfectly into the rows/columns
+            if (i % columnCount == 0)
+                j++;
+
+            //create a new item, name it, and set the parent
+            GameObject newItem = Instantiate(itemPrefab) as GameObject;
+            newItem.name = gameObject.name + " item at (" + i + "," + j + ")";
+            //newItem.transform.parent = gameObject.transform;
+			newItem.transform.SetParent (gameObject.transform);
+            //move and size the new item
+            RectTransform rectTransform = newItem.GetComponent<RectTransform>();
+			newItem.GetComponentInChildren<Text>().text = descs[i].name;
+            float x = -containerRectTransform.rect.width / 2 + width * (i % columnCount);
+            float y = containerRectTransform.rect.height / 2 - height * j;
+            rectTransform.offsetMin = new Vector2(x, y);
+
+            x = rectTransform.offsetMin.x + width;
+            y = rectTransform.offsetMin.y + height;
+            rectTransform.offsetMax = new Vector2(x, y);
+        }
+
+
+    }
+
+}
--- Assets/Scripts/UIScripts/UIMaster.cs
+++ Assets/Scripts/UIScripts/UIMaster.cs
@@ -5,31 +5,33 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class UIMaster : MonoBehaviour {
-	public GameObject oneObjective;
 	ArrayList children;
 	GameObject theobj;
 	public List<Objective> descriptions;
+	public ScrollableList scrolist;
 
 	void Start(){
 		descriptions = new List<Objective>();
 		Enemy anenemy = GameObject.Find ("/Enemy").GetComponent<Enemy> (); //find whatever enemy is first in the scene
 		UnityEngine.Debug.Log (anenemy);
 		descriptions.Add ( new Objective ("Kill Him", "He Needs To Die", "kill",anenemy, new Vector3(0,0,0)));
+		scrolist = this.GetComponent<ScrollableList> ();
 	}
 
 	void Update(){
-		System.Diagnostics.Debug.Assert (descriptions != null);
-		foreach (Objective o in descriptions) {
-			if(o.isObjectiveDone ()){
-				descriptions.Remove(o);
-			}
-			UnityEngine.Debug.Log ("Enemy is "+ o.evil.distanceToPlayer);
+				System.Diagnostics.Debug.Assert (descriptions != null);
+				foreach (Objective o in descriptions) {
+						if (o.isObjectiveDone ()) {
+								descriptions.Remove (o);
+						}
+						//UnityEngine.Debug.Log ("Enemy is "+ o.evil.distanceToPlayer);
+				}
+	
+		
+		if (Input.GetKeyDown ("tab")) {
+			scrolist.doTheGUI ();
 		}
-	}
-		/*
-		if(Input.GetKeyDown("tab")){
-			objectives ();
-		}
+				
 		if (Input.GetKeyUp ("tab")) {
 			children = new ArrayList ();
 			foreach (Transform child in transform)
@@ -38,23 +40,6 @@ public class UIMaster : MonoBehaviour {
 					Destroy (child);
 			}
 		}
-	}
-	 */
-	void objectives(){
-			/*
-		float x = 350;
-		float y = 0;
-		foreach (Objective obj in descriptions) {
-			theobj = Instantiate (oneObjective) as GameObject;
-			theobj.transform.parent = gameObject.transform;
-			RectTransform rectTransform = theobj.GetComponent<RectTransform> ();
-			rectTransform.offsetMin = new Vector2 (x, y);
-			rectTransform.offsetMax = new Vector2 (x + 60, y - 50);
-			theobj.GetComponentInChildren<Text> ().text = obj.description;
-			y -= 50;
-
-		}
-	*/
 	}
 	
 }
