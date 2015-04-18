b291abbbdadee68028f4f2b8fb1e17bdcc282646
Reddeyfish <edrichd@uci.edu>
2015-04-17 22:38:37 -0700

    saving settings

---
+++ Assets/Scripts/EnvironmentScripts/Tags.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Options : MonoBehaviour
+{
+    public const string MusicLevel = "MusicLevel";
+}
---
+++ Assets/Scripts/UIScripts/SliderBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderBehaviour : MonoBehaviour {
+    public string optionString;
+    private Text SliderPercentLabel;
+    private Slider slider;
+	// Use this for initialization
+	void Start () {
+        SliderPercentLabel = transform.Find("Handle Slide Area/Handle/Text").GetComponent<Text>();
+        slider = GetComponent<Slider>();
+        if (PlayerPrefs.HasKey(optionString))
+            slider.value = PlayerPrefs.GetInt(optionString)/100.0f;
+	}
+
+    public void SliderUpdate()
+    {
+        int newValue = (int)(100*slider.value);
+        SliderPercentLabel.text = (newValue).ToString() + '%';
+        PlayerPrefs.SetInt(optionString, newValue);
+        PlayerPrefs.Save();
+    }
+}
