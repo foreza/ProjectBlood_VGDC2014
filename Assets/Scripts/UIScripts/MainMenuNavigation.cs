a1d7e99412d2f2995cfd9d6a238f40e1fb50c069
Cheddar2012 <cheddar1@outlook.com>
2015-05-03 18:27:17 -0700

    Main menu now only one scene; audio in main menu

---
+++ Assets/Scripts/UIScripts/MainMenuNavigation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuNavigation : MonoBehaviour {
+
+	public void LoadCanvas(GameObject callingCanvas, GameObject canvasToLoad) {
+		callingCanvas.SetActive(false);
+		canvasToLoad.SetActive(true);
+	}
+}
