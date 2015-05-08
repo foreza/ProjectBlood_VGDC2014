5f99be8b230cfff077be1fda32747f4e7559a9f4
Reddeyfish <edrichd@uci.edu>
2015-05-08 16:52:50 -0700

    forgot the script

--- Assets/Scripts/EnemyScripts/TreeHoming.cs
+++ Assets/Scripts/EnemyScripts/TreeHoming.cs
@@ -5,21 +5,37 @@ public class TreeHoming : MonoBehaviour {
     private Player player;
     private Rigidbody2D rigid;
     public float acceleration = 20000.0f;
-    public float duration = 50.0f;
     public float damage = 25.0f;
+    private GameObject leshy;
+    private bool summoned = false;
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player").GetComponent<Player>();
+        leshy = GameObject.Find("Leshy");
         rigid = this.GetComponent<Rigidbody2D>();
-        Destroy(this.gameObject, duration);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        rigid.AddForce(Time.deltaTime * acceleration * ((Vector2)(player.transform.position - this.transform.position)).normalized); //accelerate to the player
-        if (rigid.velocity.magnitude < 1)
+        if (summoned)
         {
-            rigid.velocity = rigid.velocity.normalized;
+            if ((leshy.transform.position - this.transform.position).magnitude < 5)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                rigid.AddForce(Time.deltaTime * acceleration * ((Vector2)(leshy.transform.position - this.transform.position)).normalized); //gets pulled to leshy
+            }
+            
+        }
+        else
+        {
+            rigid.AddForce(Time.deltaTime * acceleration * ((Vector2)(player.transform.position - this.transform.position)).normalized); //accelerate to the player
+            if (rigid.velocity.magnitude < 1)
+            {
+                rigid.velocity = rigid.velocity.normalized;
+            }
         }
 	}
 
@@ -30,5 +46,15 @@ public class TreeHoming : MonoBehaviour {
             other.gameObject.GetComponent<Player>().takeHit(damage); // We should take damage from the enemy.
         }
 
+    }
+
+    public void Summon()
+    {
+        summoned = true;
+        //disable collision
+        foreach (Collider2D c in GetComponentsInChildren<Collider2D>())
+        {
+            c.enabled = false;
+     } 
     }
 }
