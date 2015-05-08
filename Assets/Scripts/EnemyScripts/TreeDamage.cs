b707be97c9a3a081ddf98e9e297fccbf4550095c
Reddeyfish <edrichd@uci.edu>
2015-05-08 16:06:44 -0700

    Leshy's tree branches do damage to player

---
+++ Assets/Scripts/EnemyScripts/TreeDamage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeDamage : MonoBehaviour {
+
+    public float damage = 50.0f;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Player>().takeHit(damage); // We should take damage from the enemy.
+        }
+
+    }
+}
--- Assets/Scripts/EnemyScripts/TreeHoming.cs
+++ Assets/Scripts/EnemyScripts/TreeHoming.cs
@@ -6,6 +6,7 @@ public class TreeHoming : MonoBehaviour {
     private Rigidbody2D rigid;
     public float acceleration = 20000.0f;
     public float duration = 50.0f;
+    public float damage = 25.0f;
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player").GetComponent<Player>();
@@ -16,5 +17,18 @@ public class TreeHoming : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         rigid.AddForce(Time.deltaTime * acceleration * ((Vector2)(player.transform.position - this.transform.position)).normalized); //accelerate to the player
+        if (rigid.velocity.magnitude < 1)
+        {
+            rigid.velocity = rigid.velocity.normalized;
+        }
 	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Player>().takeHit(damage); // We should take damage from the enemy.
+        }
+
+    }
 }
