53e26e213c43300d3c3211e194aaf9b85bd9158f
Rex Rockwell <rexjrockwell@outlook.com>
2015-05-26 20:18:33 -0700

    Disable exit buttons in web player

---
+++ Assets/Scripts/WebDisable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Attach this script to game objects that should be absent in web player
+public class WebDisable : MonoBehaviour {
+
+	void Start () {
+        if (Application.platform == RuntimePlatform.WindowsWebPlayer ||
+            Application.platform == RuntimePlatform.OSXWebPlayer ||
+            Application.platform == RuntimePlatform.WebGLPlayer) {
+            gameObject.SetActive(false);
+        }
+	}
+	
+}
